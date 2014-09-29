f1864e4d9dcb2ef87c49a4a47de17b14650d0b78
Feifan Zhou <feifan@me.com>
2014-09-29 19:05:33 -0400

    Refactor drawing code into separate methods and draw vertical line for each node

--- Equalizer/EQVisualizer.m
+++ Equalizer/EQVisualizer.m
@@ -10,6 +10,11 @@
 #import "FunctionalNSArray.h"
 #import "EQPoint.h"
 
+@interface EQVisualizer () {
+	CGFloat _baselineY;
+}
+@end
+
 @implementation EQVisualizer
 
 # pragma mark -
@@ -29,7 +34,9 @@
 }
 
 - (CGFloat)baselineY {
-	return (2.0/3.0) * [self bounds].size.height;
+	if (_baselineY <= 0)
+		_baselineY = (2.0/3.0) * [self bounds].size.height;
+	return _baselineY;
 }
 
 #pragma mark -
@@ -47,11 +54,10 @@
 	return self;
 }
 
-- (void)drawRect:(NSRect)dirtyRect {
-    [super drawRect:dirtyRect];
-	
-	CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort];
-	// Draw baseline first
+# pragma mark -
+# pragma mark Drawing methods
+
+- (void)drawBaseline:(CGContextRef)context {
 	CGFloat baselineY = [self baselineY];
 	CGContextBeginPath(context);
 	CGContextSetRGBStrokeColor(context, 0, 0, 0, 0.3);
@@ -60,28 +66,50 @@
 	CGContextAddLineToPoint(context, ([self bounds].size.width - 10), baselineY);
 	CGContextClosePath(context);
 	CGContextDrawPath(context, kCGPathStroke);
+}
+- (void)drawDotAtPoint:(NSPoint)point inContext:(CGContextRef)context {
+	CGRect circleRect = CGRectMake(point.x - 5, point.y, 10.0, 10.0);
+	CGContextSetLineWidth(context, 1.0);
+	CGContextAddEllipseInRect(context, circleRect);
+	CGContextDrawPath(context, kCGPathStroke);
+	
+	// Draw vertical line
+	CGContextBeginPath(context);
+	CGContextSetRGBStrokeColor(context, 0, 0, 0, 0.1);
+	CGContextSetLineWidth(context, 1.0);
+	CGContextMoveToPoint(context, point.x, 10);
+	CGContextAddLineToPoint(context, point.x, [self bounds].size.height - 10);
+	CGContextClosePath(context);
+	CGContextDrawPath(context, kCGPathStroke);
+}
+- (void)drawPoint:(EQPoint *)point inContext:(CGContextRef)context {
+	CGContextBeginPath(context);
+	if (point.isSelected)
+		CGContextSetRGBStrokeColor(context, 1, 0, 0, 1.0);
+	else
+		CGContextSetRGBStrokeColor(context, 0, 0, 0, 1.0);
+	[self drawDotAtPoint:point.location inContext:context];
+}
+- (void)drawHoverPoint:(NSPoint)point inContext:(CGContextRef)context {
+	CGContextSetRGBStrokeColor(context, 0, 0, 0, 0.25);
+	NSPoint modifiedPoint = NSMakePoint(point.x, [self baselineY] - 5);
+	[self drawDotAtPoint:modifiedPoint inContext:context];
+}
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+	
+	CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort];
+	// Draw baseline first
+	[self drawBaseline:context];
 	
 	// Draw points
-	for (EQPoint *point in self.eqPoints) {
-		CGRect circleRect = CGRectMake(point.location.x - 5, baselineY/* point.location.y */- 5, 10.0, 10.0);
-		CGContextBeginPath(context);
-		if (point.isSelected)
-			CGContextSetRGBStrokeColor(context, 1, 0, 0, 1.0);
-		else
-			CGContextSetRGBStrokeColor(context, 0, 0, 0, 1.0);
-		CGContextSetLineWidth(context, 1.0);
-		CGContextAddEllipseInRect(context, circleRect);
-		CGContextDrawPath(context, kCGPathStroke);
-	}
+	for (EQPoint *point in self.eqPoints)
+		[self drawPoint:point inContext:context];
 	
 	// Draw mouseover point if needed
 	if (self.mouseoverPoint.x < 0 && self.mouseoverPoint.y < 0)
 		return;
-	CGRect circleRect = CGRectMake(self.mouseoverPoint.x - 5, baselineY - 5, 10.0, 10.0);
-	CGContextBeginPath(context);
-	CGContextSetRGBStrokeColor(context, 0, 0, 0, 0.25);
-	CGContextAddEllipseInRect(context, circleRect);
-	CGContextDrawPath(context, kCGPathStroke);
+	[self drawHoverPoint:self.mouseoverPoint inContext:context];
 }
 
 # pragma mark - 
@@ -95,7 +123,7 @@
 	}];
 	self.eqPoints = [NSMutableArray arrayWithArray:deselectedArray];
 	NSPoint center = [self pointFromMouseEvent:theEvent];
-	center.y = [self baselineY];
+	center.y = [self baselineY] - 5;
 	[self.eqPoints addObject:[[EQPoint alloc] initWithPoint:center]];
 	[self setNeedsDisplay:YES];
 }
